cfb37be4030196a1af02cb69783aec98c0920711
Meruem Moniker <jamesconnorhill@gmail.com>
2024-03-19 08:32:01 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Connor Hill on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
